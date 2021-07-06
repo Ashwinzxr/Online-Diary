c8f0621ecc46fe2b19c681f05bda53be6c30f4dd
suren <sibiram7@gmail.com>
2021-07-06 19:58:50 +0530

    hello

--- sip.php
+++ sip.php
@@ -11,6 +11,29 @@ if ($conn->connect_error)
         die("Connection failed: " . $conn->connect_error);
 }
 $error = " " ; 
+$mail=$_POST["email"]; 
+$usname=$_POST["uname"]; 
+$passw=$_POST["pass"]; 
+$rpassw=$_POST["rpass"]; 
+if( $passw!=$rpassw) 
+{ 
+        echo "password not matching"; 
+} 
+else 
+{ 
+        $sql = "CREATE TABLE $usname 
+        ( id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+        email VARCHAR(70) NOT NULL UNIQUE, fname TEXT NOT NULL 
+        )"; 
+        if(mysqli_query($conn, $sql)) 
+        { 
+                echo "Table created successfully."; 
+        } 
+        else 
+        {
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn); 
+        } 
+}
 ?>
 
 
