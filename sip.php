ce64b4a497e9c3f747bca5fdad4eb745881ea4fa
suren <sibiram7@gmail.com>
2021-07-07 19:45:07 +0530

    hello

--- sip.php
+++ sip.php
@@ -15,25 +15,54 @@ $mail=$_POST["email"];
 $usname=$_POST["uname"]; 
 $passw=$_POST["pass"]; 
 $rpassw=$_POST["rpass"]; 
+$file="login.html";
 if( $passw!=$rpassw) 
 { 
         echo "password not matching"; 
 } 
 else 
-{ 
-        $sql = "CREATE TABLE $usname 
-        ( id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-        email VARCHAR(70) NOT NULL UNIQUE, fname TEXT NOT NULL 
-        )"; 
-        if(mysqli_query($conn, $sql)) 
-        { 
-                echo "Table created successfully."; 
-        } 
-        else 
+{       
+    $sq = "SELECT * FROM users where username='$usname'";
+    $query = mysqli_query($conn,$sq);
+    if(mysqli_num_rows($query))
+    {
+      echo "username is present";
+    }
+    else
+    {  
+        $in = "INSERT INTO users(email,username,password)
+                VALUES ('$mail','$usname','$passw')";
+
+        if ($conn->query($in) === FALSE) 
         {
-                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn); 
-        } 
+                echo "Error: " . $in . "<br>" . $conn->error;
+        }       
+        else    
+        {
+                $sql = "CREATE TABLE $usname(
+                id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+                filen TEXT NOT NULL,
+                email VARCHAR(70) NOT NULL UNIQUE        
+                )"; 
+                if(mysqli_query($conn, $sql)) 
+                { 
+                        echo "<html>";
+                        echo "<a href='login.html'><button>login</button></a>";
+                        //echo "<button>LOGIN</button>";
+                        //echo "<html> <body><h1>ACCOUNT CREATED SUCESSFULLY</h1><br><button onclick="location.href='login.html'">LOGIN PAGE</button></body></html>"; 
+                        echo "</html>";
+                } 
+                else 
+                {
+                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn); 
+                }  
+        }
+    }
+        
 }
+$conn->close();
 ?>
+       
+
 
 
