2ca7679b73ffd26c245c76bb77dbdb687a83ea52
Ashwinzxr <ashwin.achchu@gmail.com>
2021-07-11 18:11:13 +0530

    s

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const par1 = document.getElementById("one") ;
 const par3 = document.getElementById("three") ;
 window.addEventListener("scroll",function() {
 	let offset = window.pageYOffset ;
-	par1.style.backgroundPositionY= offset*-1.8 +"px" ;
-	par3.style.backgroundPositionY= offset*-1.8 +"px" ;
+	par1.style.backgroundPositionY= offset*-2 +"px" ;
+	par3.style.backgroundPositionY= offset*-2 +"px" ;
 })
