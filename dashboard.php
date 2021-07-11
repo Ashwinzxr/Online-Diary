2d4bea6ce6bba9e24383a761aaa0a0b521ac7d84
suren <sibiram7@gmail.com>
2021-07-11 22:10:56 +0530

    hello

--- dashboard.php
+++ dashboard.php
@@ -16,9 +16,10 @@ error_reporting(E_ALL);
 $file=$_POST['inp'];
 $username=$_SESSION['uname'];
 $filename = "diary".date("Y-m-d").".txt";
+$path = "Source/".$filename;
 $dof=date("y/m/d");
 
-$diary = fopen($filename, "a");
+$diary = fopen($path, "a");
 fwrite($diary,$file);
 
 $query= "INSERT INTO $username (filen,dof) VALUES('$filename','$dof') ";
